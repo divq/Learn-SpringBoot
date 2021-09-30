4d67fb0ccdbe0ae697ccc8cc72d9ce41d45ab8dd
divq <idivq@outlook.com>
2021-09-30 17:38:39 +0800

    04_data initial commit

---
+++ springboot_04_data/src/test/java/com/example/springboot_04_data/Springboot04DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_04_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04DataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
