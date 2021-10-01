42c71476ff6bd81ff797733d1a38b3c4677a43f7
divq <idivq@outlook.com>
2021-10-01 10:10:45 +0800

    06-SpringSecurity initial commit

---
+++ springboot_06_springsecurity/src/test/java/com/example/springboot_06_springsecurity/Springboot06SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_06_springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
