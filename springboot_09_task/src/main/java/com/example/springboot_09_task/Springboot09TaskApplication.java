da21a5de37550cc0a924d00e8cf7f622bb02871a
divq <idivq@outlook.com>
2021-10-02 14:56:03 +0800

    09 task initial commit

---
+++ springboot_09_task/src/main/java/com/example/springboot_09_task/Springboot09TaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_09_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09TaskApplication.class, args);
+    }
+
+}
